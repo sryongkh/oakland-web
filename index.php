34409649d4be2dc21a6956856c54f62401744226
Sirinya <sry.ongkh@gmail.com>
2024-03-24 04:41:30 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php $root = "";?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Oakland | Energy Consulting</title>
+    <!-- ICON -->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php $root; ?>assets/icon/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php $root; ?>assets/icon/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php $root; ?>assets/icon/favicon-16x16.png" />
+    <link rel="manifest" href="<?php $root; ?>assets/icon/site.webmanifest" />
+    <link rel="mask-icon" href="<?php $root; ?>assets/icon/safari-pinned-tab.svg" color="#212529" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#212529" />
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php $root; ?>assets/css/" />
+    <link rel="stylesheet" href="<?php $root; ?>assets/font/font.css" />
+  </head>
+  <body>
+    <header></header>
+    <main>
+        <section></section>
+    </main>
+    <footer></footer>
+    <!-- JS -->
+    <script src="/assets/js/theme.js"></script>
+  </body>
+</html>
