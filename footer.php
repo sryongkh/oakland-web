74dfe2978b1ceab454bf1d59ddcea9df2932b1f7
Sirinya <sry.ongkh@gmail.com>
2024-03-26 04:50:52 +0700

    Home page

--- footer.php
+++ footer.php
@@ -1,6 +1,34 @@
 <?php $root = ""; ?>
 
-<footer></footer>
+<footer>
+    <div class="container">
+        <div class="wrapper">
+            <div class="menu">
+                <div class="page-menu">
+                    <p class="">Pages</p>
+                    <a href="">Services</a>
+                    <a href="">About us</a>
+                    <a href="">Portfolio</a>
+                    <a href="">Contact us</a>
+                </div>
+                <div class="social-media-menu">
+                    <p class="">Social Media</p>
+                    <a href="">Facebook</a>
+                    <a href="">Linkedin</a>
+                </div>
+                <div class="legal-menu">
+                    <p class="">Legal</p>
+                    <a href="">Privacy and Cookies Policy</a>
+                    <a href="">Terms and Condition</a>
+                </div>
+            </div>
+            <div class="iso">
+                <img src="<?php $root; ?>assets/media/iso.png" alt="">
+            </div>
+        </div>
+        <p class="credit">Oakland - Energy Consulting ©</p>
+    </div>
+</footer>
 <!-- JS -->
 <script src="<?php $root; ?>assets/js/theme.js"></script>
 </body>
