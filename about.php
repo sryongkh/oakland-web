9b8e80f040069324113d74ff126082a0286e5940
Sirinya <sry.ongkh@gmail.com>
2024-03-30 01:39:33 +0700

    Update about: responsive

--- about.php
+++ about.php
@@ -10,7 +10,7 @@
             </div>
         </div>
     </section>
-    <section data-section="contxt-1" class="">
+    <section data-section="contxt-1">
         <h2 class="fluid-heading-2">Innovation.<br />
             Guiding our journey from the start.</h2>
         <div class="img-container">
@@ -20,16 +20,16 @@
                 <img src="<?php $root; ?>assets/media/about-1.png" alt="">
             </figure>
         </div>
-        <div class="paragraph">
+        <div class="paragraph fluid-body">
             <p>Innovation has been at the heart of our journey right from the beginning. At Oakland C.E., we believe that staying at the forefront of technology and sustainable practices is essential. We've consistently sought innovative solutions in energy efficiency, LED lighting, photovoltaic energy, and electric mobility to shape a brighter and greener future.</p>
             <br />
             <p>Our commitment to innovation drives us to continually explore new avenues, refine our processes, and deliver pioneering results for our clients. Join us in embracing innovation as we chart a course towards a more sustainable and efficient energy landscape.</p>
         </div>
     </section>
-    <section data-section="contxt-2" class="">
+    <section data-section="contxt-2">
         <div class="banner">
         </div>
-        <div class="paragraph">
+        <div class="paragraph fluid-body">
             <p>From our company's inception, innovation has been ingrained in our DNA. It's not just a buzzword; it's the driving force behind everything we do. Our founders were visionaries who understood that to make a real impact in the energy sector, we needed to lead with innovation.</p>
             <br />
             <p>This commitment to cutting-edge solutions has allowed us to consistently push the boundaries of what's possible in energy efficiency, LED lighting, photovoltaic energy, and electric mobility. We've fostered a culture that encourages creativity, embraces change, and thrives on challenges. As we continue to grow, our dedication to innovation remains unwavering, ensuring that we remain a trailblazer in the industry.</p>
