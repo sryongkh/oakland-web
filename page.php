02b208ab325ca63d1f7f0ef442bb5b631814ed5a
Sirinya <sry.ongkh@gmail.com>
2024-03-30 19:16:58 +0700

    Update privacy page

--- page.php
+++ page.php
@@ -2,12 +2,44 @@
 
 <?php include($root . "header.php"); ?>
 <main>
-    <section data-section="privacy-hero" class="header white">
+    <section data-section="privacy-hero" class="header light">
         <div class="container">
             <div class="contxt">
                 <h2 class="fluid-heading-1">Privacy and Cookies Policy</h2>
                 <p class="fluid-body">This privacy is designed to inform you about how we collect, use, and<br />protect your personal information when you interact with our website.</p>
             </div>
+        </div>
+    </section>
+    <section data-section="policy-list">
+        <div class="list">
+            <h2 class="fluid-heading-2">1. Information we collect</h2>
+            <p class=s"fluid-body">
+                We gather various types of information to enhance and optimize our services. This includes personal information such as your name, email address, phone number, and mailing address, collected when you register for an account, contact us, or use our services. We also gather usage information, which encompasses details about your interactions with our website and services, including your IP address, browser type, device information, pages visited, and actions taken.
+            </p>
+        </div>
+        <div class="list">
+            <h2 class="fluid-heading-2">2. How we use your information</h2>
+            <p class=s"fluid-body">
+                Our utilization of the information we collect serves multiple purposes. We use your personal data to furnish requested services, such as responding to inquiries, processing orders, and delivering newsletters or updates if you opt to receive them. Additionally, we analyze usage information to gain insights into how our website and services are used, which informs our continuous improvement efforts, content customization, and enhancements to user experiences.
+            </p>
+        </div>
+        <div class="list">
+            <h2 class="fluid-heading-2">3. Communication</h2>
+            <p class=s"fluid-body">
+                We may employ your contact information to communicate with you concerning our services, provide updates, and convey important notices. You have the option to opt out of marketing communications at any time.
+            </p>
+        </div>
+        <div class="list">
+            <h2 class="fluid-heading-2">4. Cookies and Tracking Technologies</h2>
+            <p class=s"fluid-body">
+                To better understand your browsing behavior and improve your website experience, we utilize cookies and similar tracking technologies. Cookies are small data files stored on your device that facilitate our comprehension of your preferences. By utilizing our website, you implicitly consent to our use of cookies, as detailed in our Cookies Policy. You maintain the ability to manage your cookie preferences through your browser settings.
+            </p>
+        </div>
+        <div class="list">
+            <h2 class="fluid-heading-2">5. Security</h2>
+            <p class=s"fluid-body">
+                We prioritize data security and employ reasonable measures to safeguard your information from unauthorized access, disclosure, alteration, or destruction. While we diligently strive to protect your data, it's important to acknowledge that no method of transmission over the internet or electronic storage is entirely secure. Consequently, we cannot provide absolute security guarantees.
+            </p>
         </div>
     </section>
 </main>
