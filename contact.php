3504fa98219c095b3f02ff0817f462c0b1e3e198
Sirinya <104513585+sryongkh@users.noreply.github.com>
2024-04-08 16:04:38 +0700

    add Barba in contact page

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php $root = ""; ?>
 
 <?php include($root . "header.php"); ?>
-<main>
+<main data-barba="container" data-barba-namespace="home">
     <section data-section="contact-hero" class="header light">
         <div class="container">
             <div class="contxt">
